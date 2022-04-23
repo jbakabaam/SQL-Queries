620d535987b2f41b8028a545da092ca254bd9ac5
Jun Buhm Lee <103108988+jbakabaam@users.noreply.github.com>
2022-04-23 19:04:02 +0900

    Update AverageSalaries.sql

--- StrataScratch/AverageSalaries.sql
+++ StrataScratch/AverageSalaries.sql
@@ -1,5 +1,5 @@
 '''
-Compare each employee's salary with the average salary of the corresponding department.
+Compare each employee\'s salary with the average salary of the corresponding department.
 Output the department, first name, and salary of employees along with the average salary of that department.
 '''
 
