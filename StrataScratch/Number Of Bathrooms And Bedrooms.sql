cd257e71b3fd775d7c7dcbb870c8e3d0f0fb12c5
Jun Buhm Lee <103108988+jbakabaam@users.noreply.github.com>
2022-04-23 16:06:07 +0900

    Update Number Of Bathrooms And Bedrooms.sql

--- StrataScratch/Number Of Bathrooms And Bedrooms.sql
+++ StrataScratch/Number Of Bathrooms And Bedrooms.sql
@@ -1,5 +1,5 @@
 '''
-Find the average number of bathrooms and bedrooms for each city/'s property types.
+Find the average number of bathrooms and bedrooms for each city\'s property types.
 Output the result along with the city name and the property type.
 '''
 
