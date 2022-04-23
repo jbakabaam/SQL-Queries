3f547bd6ab36eb942d80de2c88fa20045661ff6d
Jun Buhm Lee <103108988+jbakabaam@users.noreply.github.com>
2022-04-23 16:05:21 +0900

    Create Number Of Bathrooms And Bedrooms.sql

---
+++ StrataScratch/Number Of Bathrooms And Bedrooms.sql
@@ -0,0 +1,9 @@
+'''
+Find the average number of bathrooms and bedrooms for each city/'s property types.
+Output the result along with the city name and the property type.
+'''
+
+SELECT city, property_type, AVG(bathrooms), AVG(bedrooms)
+FROM airbnb_search_details
+GROUP BY 1,2
+;
