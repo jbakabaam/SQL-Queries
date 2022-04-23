0d18a5a353edb9868a42eafe6b173d8c2be5def8
Jun Buhm Lee <103108988+jbakabaam@users.noreply.github.com>
2022-04-23 16:01:50 +0900

    Update Finding Updated Records.sql

--- StrataScratch/Finding Updated Records.sql
+++ StrataScratch/Finding Updated Records.sql
@@ -1,7 +1,9 @@
+'''
 We have a table with employees and their salaries, however, some of the records are old and contain outdated salary information.
 Find the current salary of each employee assuming that salaries increase each year.
 Output their id, first name, last name, department ID, and current salary.
 Order your list by employee ID in ascending order.
+'''
 
 SELECT id, first_name, last_name, department_id, max(salary)
 FROM ms_employee_salary m
