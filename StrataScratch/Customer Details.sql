c9d3eaa582737a5028e27e769c2e029b9a6d7ad1
Jun Buhm Lee <103108988+jbakabaam@users.noreply.github.com>
2022-04-23 16:04:07 +0900

    Update Customer Details.sql

--- StrataScratch/Customer Details.sql
+++ StrataScratch/Customer Details.sql
@@ -1,8 +1,8 @@
 '''
 Find the details of each customer regardless of whether the customer made an order.
-Output the customer's first name, last name, and the city along with the order details.
+Output the customer\'s first name, last name, and the city along with the order details.
 You may have duplicate rows in your results due to a customer ordering several of the same items.
-Sort records based on the customer's first name and the order details in ascending order.
+Sort records based on the customer\'s first name and the order details in ascending order.
 '''
 
 SELECT c.first_name, c.last_name, c.city, o.order_details
