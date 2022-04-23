057fc5398d1e9fa98eb43ab9bb6b3daf45e3c61c
Jun Buhm Lee <103108988+jbakabaam@users.noreply.github.com>
2022-04-23 15:58:45 +0900

    Create Salaries Differences.sql

---
+++ StrataScratch/Salaries Differences.sql
@@ -0,0 +1,22 @@
+Write a query that calculates the difference between the highest salaries found in the marketing and engineering departments.  
+Output just the absolute difference in salaries. 
+
+'''
+SELECT
+(
+SELECT MAX(salary)
+FROM db_employee e
+JOIN db_dept d
+ON e.department_id = d.id
+WHERE department = 'marketing'
+)
+-
+(
+SELECT MAX(salary)
+FROM db_employee e
+JOIN db_dept d
+ON e.department_id = d.id
+WHERE department = 'engineering'
+) AS answer
+;
+'''
