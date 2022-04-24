92a8e119d6486e0974e5047a36c0d2af84719fd7
Jun Buhm Lee <103108988+jbakabaam@users.noreply.github.com>
2022-04-24 13:39:05 +0900

    Create Find the average score for grades A, B, and C.sql

---
+++ StrataScratch/Find the average score for grades A, B, and C.sql
@@ -0,0 +1,8 @@
+'''
+Find the average score for grades A, B, and C. Output the results along with the corresponding grade (ex: 'A', avg(score)).
+'''
+
+SELECT grade, AVG(score)
+FROM los_angeles_restaurant_health_inspections
+GROUP BY 1
+;
