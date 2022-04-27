90a49a945181f1d8fb06101ed71984cab63c4480
Jun Buhm Lee <103108988+jbakabaam@users.noreply.github.com>
2022-04-27 10:21:31 +0900

    Create Top Earners.sql

---
+++ HackerRank/Top Earners.sql
@@ -0,0 +1,15 @@
+'''
+We define an employee's total earnings to be their monthly  worked, and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings.
+Then print these values as  space-separated integers.
+
+where employee_id is an employee's ID number, name is their name, months is the total number of months they've been working for the company, and salary is the their monthly salary.
+
+The maximum earnings value is Kimberly. The only employee with earnings is Kimberly, so we print the maximum earnings value () and a count of the number of employees who have earned  (which is ) as two space-separated values.
+'''
+
+SELECT salarymonths AS earnings, COUNT(*)
+FROM employee
+GROUP BY 1
+ORDER BY earnings DESC
+LIMIT 1
+;
