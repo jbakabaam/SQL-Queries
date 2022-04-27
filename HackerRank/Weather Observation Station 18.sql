c71834b69628b9fcb55464187999b993aca9f8c7
Jun Buhm Lee <103108988+jbakabaam@users.noreply.github.com>
2022-04-27 10:08:14 +0900

    Update Weather Observation Station 18.sql

--- HackerRank/Weather Observation Station 18.sql
+++ HackerRank/Weather Observation Station 18.sql
@@ -6,7 +6,7 @@ Consider  P1(a, b) and P2(c, d) to be two points on a *2D* plane.
 - c happens to equal the maximum value in *Northern Latitude* (*LAT_N* in **STATION**).
 - d happens to equal the maximum value in *Western Longitude* (*LONG_W* in **STATION**).
 
-Query the [Manhattan Distance](https://xlinux.nist.gov/dads/HTML/manhattanDistance.html) between points  and  and round it to a scale of  decimal places.
+Query the \'Manhattan Distance\' between points  and  and round it to a scale of  decimal places.
 '''
 
 SELECT ROUND(ABS(MIN(LAT_N)-MAX(LAT_N))+ABS(MIN(LONG_W)-MAX(LONG_W)), 4)
