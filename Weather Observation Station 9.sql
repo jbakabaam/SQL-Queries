36e612632a46f9887967eeae208bab970e7330d6
Jun Buhm Lee <103108988+jbakabaam@users.noreply.github.com>
2022-04-27 10:01:19 +0900

    Delete Weather Observation Station 9.sql

--- Weather Observation Station 9.sql
+++
@@ -1,9 +0,0 @@
-'''
-Query the list of CITY names from STATION that do not start with vowels.
-Your result cannot contain duplicates.
-'''
-
-SELECT DISTINCT city
-FROM station
-WHERE NOT city REGEXP '^[aeiou]'
-;
