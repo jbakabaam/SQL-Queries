50ac20de2d4c5d549922103a4862b65d36600ce3
Jun Buhm Lee <103108988+jbakabaam@users.noreply.github.com>
2022-04-27 10:05:48 +0900

    Create Weather Observation Station 16.sql

---
+++ HackerRank/Weather Observation Station 16.sql
@@ -0,0 +1,11 @@
+'''
+Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 38.7780.
+Round your answer to  decimal places.
+'''
+
+SELECT ROUND(lat_n,4)
+FROM station
+WHERE lat_n > 38.7780
+ORDER BY lat_n ASC
+LIMIT 1
+;
