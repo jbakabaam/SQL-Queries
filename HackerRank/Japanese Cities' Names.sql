e7bc1865b87da4720c532b22f270dfa6cf8bd8e6
Jun Buhm Lee <103108988+jbakabaam@users.noreply.github.com>
2022-04-27 09:42:57 +0900

    Create Japanese Cities' Names.sql

---
+++ HackerRank/Japanese Cities' Names.sql
@@ -0,0 +1,9 @@
+'''
+Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.
+The CITY table is described as follows:
+'''
+
+SELECT name
+FROM city
+WHERE countrycode='JPN'
+;
